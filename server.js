76ac6d1da96be787326be3454e70c428bb37bfa8
kodykoester <105684121+kodykoester@users.noreply.github.com>
2023-12-02 05:34:10 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productmodels.js')
 const app = express()
+
+
+app.use(express.json())
+
 
 // routes
 app.get('/', (req, res) => {
     res.send('hello')
+})
+
+app.post('/product',async (req, res) => {
+    try {
+const product =  await Product.create(req.body)
+res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
 })
 
 
@@ -15,7 +30,7 @@ mongoose.connect('mongodb+srv://admin:admin@gng.g9rq1cs.mongodb.net/?retryWrites
     app.listen(3000, ()=> {
         console.log("hello, again.")
     })
-    console.log('connectd to MongoDB')
+    console.log('connected to MongoDB')
 }).catch((error) => {
     console.log(error)
 })
